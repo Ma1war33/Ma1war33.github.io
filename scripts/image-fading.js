76e117a693bd56e5c96569a1413b3519f537c8cd
Ma1war3 <78973197+Ma1war33@users.noreply.github.com>
2022-03-03 10:54:48 +0000

    Add files via upload

--- scripts/image-fading.js
+++ scripts/image-fading.js
@@ -1,15 +1,50 @@
 var morsecoderimg = document.getElementById("morsecoderimg");
 var wordleimg = document.getElementById("wordleimg");
 
+var showimages = true
+
+if (window.innerWidth < 1250) {
+    showimages = false
+    morsecoderimg.style.opacity = "0";
+    wordleimg.style.opacity = "0";
+}
+else {
+    showimages = true
+    morsecoderimg.style.opacity = "0";
+    wordleimg.style.opacity = "1";
+}
+
+function windowsize() {
+    if (window.innerWidth < 1250) {
+        showimages = false
+        morsecoderimg.style.opacity = "0";
+        wordleimg.style.opacity = "0";
+        console.log("Smaller")
+    }
+    else {
+        showimages = true
+        morsecoderimg.style.opacity = "0";
+        wordleimg.style.opacity = "1";
+
+        wordle
+    }
+}
+
+addEventListener("resize", windowsize)
+
 
 function morse() {
-    wordleimg.style.opacity = "1";
-    morsecoderimg.style.opacity = "0";
+    if (showimages == true) {
+        wordleimg.style.opacity = "1";
+        morsecoderimg.style.opacity = "0";
+    }
     setTimeout(wordle, 10000)
 }
 function wordle() {
+    if (showimages == true) {
     wordleimg.style.opacity = "0";
     morsecoderimg.style.opacity = "1";
+    }
     setTimeout(morse, 10000)
 }
 

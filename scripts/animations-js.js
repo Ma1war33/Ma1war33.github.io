56091c0bf4a50a45acc250f224ba6371dff7fb08
Danil E <78973197+Ma1war33@users.noreply.github.com>
2022-03-21 08:38:12 +0000

    Add files via upload

--- scripts/animations-js.js
+++ scripts/animations-js.js
@@ -1,9 +1,6 @@
 window_width = window.innerWidth;
 program_info_div = document.getElementById("program-info-div");
 
-if (window_width > 0) {
-    
-}
 
 function runonload() {
     program_info_div.style.opacity = 1;

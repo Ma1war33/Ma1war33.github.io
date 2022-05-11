6a419133c9f1dffa7e22d8615199d4f41d3aeb8a
Danil E <78973197+Ma1war33@users.noreply.github.com>
2022-05-11 23:40:06 +0100

    Add files via upload

--- scripts/animations-js.js
+++ scripts/animations-js.js
@@ -7,18 +7,31 @@ loading_page = document.getElementById("loading-page");
 main_page = document.getElementById("main_page");
 navbar = document.getElementById("navbar");
 
+loading_message = document.getElementById("loading_message");
+
+function still_loading(){
+    loading_message.style.animation = "fade-in 0.5s 0s forwards";
+}
+
+function sleep1 (){
+    loading_page.style.zIndex = "-1";
+}
 
 function runonload() {
     loading_page.style.animation = "fade-out 0.4s 0.8s forwards";
     main_page.style.animation = "large_pop_up 1s 0.8s forwards cubic-bezier(0,.91,.26,1)";
     navbar.style.animation = "large_pop_down 0.5s 0.8s forwards cubic-bezier(0,.91,.26,1)";
-    //program_info_div.style.animation = "pop-up 1s 0s forwards";
+    program_info_div.style.animation = "fade-in 1s 0s forwards";
+    setTimeout(sleep1, 1200)
 }
+
 
 window.addEventListener("load", event =>{
     isLoaded = gallery.style.content.complete && gallery.style.content.naturalHeight !== 0;
     runonload()
 });
+
+setTimeout(still_loading, 3000)
 
 function repositioning() {
     if (window.innerWidth > 839) {

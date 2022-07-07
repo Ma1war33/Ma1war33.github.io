0ac3d3342299988b90b8308764fd3bea92a2282f
Danil E <78973197+Ma1war33@users.noreply.github.com>
2022-07-07 18:40:00 +0100

    Add files via upload

--- pg-home-script.js
+++ pg-home-script.js
@@ -4,18 +4,28 @@ interval = document.getElementById("interval-sec");
 interval_btn = document.getElementById("interval-button");
 projects = document.getElementById("projects-sec");
 
-top_btn.addEventListener('click', e => {
-    interval.scrollIntoView({behavior: 'smooth'});
-})
-
-interval_btn.addEventListener('click', e => {
-    projects.scrollIntoView({behavior: 'smooth'});
-})
-
 var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
 if (isSafari == true) {
     alert("Features on this website may not work as intended on this browser, we recommend using one such as chrome.")
 } 
+
+top_btn.addEventListener('click', e => {
+    if (isSafari == true) {
+        seamless.scrollIntoView(interval, {behavior: 'smooth'})
+    }
+    else {
+        interval.scrollIntoView({behavior: 'smooth'});
+    }
+})
+
+interval_btn.addEventListener('click', e => {
+    if (isSafari == true) {
+        seamless.scrollIntoView(projects, {behavior: 'smooth'})
+    }
+    else {
+        projects.scrollIntoView({behavior: 'smooth'});
+    }
+})
 
 
 function on_resize() {

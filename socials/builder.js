0c87063442af56025b9b2f1f42014c87231520ad
Danil E <78973197+Ma1war33@users.noreply.github.com>
2022-08-08 18:23:03 +0100

    Add files via upload

--- socials/builder.js
+++ socials/builder.js
@@ -66,43 +66,46 @@ if (hash(queryString) == "1598217186") {
         var timeDiff = endTime - startTime; //in ms
         timeDiff /= 1000;
 
-        if(timeDiff >= 0.8){
-            document.body.removeChild(document.getElementById("auth-div"));
-            document.getElementById("title-div").style.opacity = "1";
+        Promise.all(Array.from(document.images).filter(img => !img.complete).map(img => new Promise(resolve => { img.onload = img.onerror = resolve; }))).then(() => {
+            console.log('images finished loading');
+        
+            if(timeDiff >= 0.8){
+                document.body.removeChild(document.getElementById("auth-div"));
+                document.getElementById("title-div").style.opacity = "1";
 
-            for (let i = 0; i < data.length; i++) {
+                for (let i = 0; i < data.length; i++) {
 
-                var link = document.createElement("a");
-                link.href = data[i].link;
-                link.classList.add("link");
-                if (data[i].link != "") {
-                    link.target = "_blank"
+                    var link = document.createElement("a");
+                    link.href = data[i].link;
+                    link.classList.add("link");
+                    if (data[i].link != "") {
+                        link.target = "_blank"
+                    }
+
+                    var div = document.createElement("div");
+                    div.classList.add("social-item");
+                    div.style.animation = `0.5s ${0.1* i}s 1 pop-in forwards ease-out`
+
+                    var div_text = document.createElement("p");
+                    div_text.innerHTML = data[i].title;
+                    div_text.classList.add("social-item-text");
+
+                    var div_image = document.createElement("img");
+                    div_image.src = data[i].image;
+                    div_image.classList.add("social-item-image");
+
+                    var div_text_div = document.createElement("div");
+                    div_text_div.classList.add("socials-text-div");
+
+                    document.getElementById("socials-div").appendChild(link).appendChild(div).appendChild(div_image)
+                    div.appendChild(div_text_div).appendChild(div_text);
+
                 }
-                
-
-                var div = document.createElement("div");
-                div.classList.add("social-item");
-                div.style.animation = `0.5s ${0.1* i}s 1 pop-in forwards ease-out`
-
-                var div_text = document.createElement("p");
-                div_text.innerHTML = data[i].title;
-                div_text.classList.add("social-item-text");
-
-                var div_image = document.createElement("img");
-                div_image.src = data[i].image;
-                div_image.classList.add("social-item-image");
-
-                var div_text_div = document.createElement("div");
-                div_text_div.classList.add("socials-text-div");
-
-                document.getElementById("socials-div").appendChild(link).appendChild(div).appendChild(div_image)
-                div.appendChild(div_text_div).appendChild(div_text);
-
             }
-        }
-        else{
-            setTimeout(waitForElement, 100);
-        }
+            else{
+                setTimeout(waitForElement, 100);
+            }
+        });
         
     }
 

a268e536aece8b44b74dce7f4b9fbb400660ec8b
Danil E <78973197+Ma1war33@users.noreply.github.com>
2022-08-10 03:56:26 +0100

    Add files via upload

--- socials/builder.js
+++ socials/builder.js
@@ -37,7 +37,7 @@ if (queryStringHash == "124b507af6dcce7e207793ae021068c20958fcfddea9e570eb22081b
 
                             var div = document.createElement("div");
                             div.classList.add("social-item");
-                            div.style.animation = `0.5s ${(0.1* i)+0.1}s 1 pop-in forwards ease-out`
+                            div.style.animation = `0.5s ${(0.1* i)+0.2}s 1 pop-in forwards ease-out`
 
                             var div_text = document.createElement("p");
                             div_text.innerHTML = data[i].title;
@@ -60,7 +60,7 @@ if (queryStringHash == "124b507af6dcce7e207793ae021068c20958fcfddea9e570eb22081b
                             document.getElementById("title-div").style.opacity = "1";
                         }
                         
-                        setTimeout(gotosocials, 100)
+                        setTimeout(gotosocials, 200)
                     }
                     else{
                         setTimeout(waitForElement, 100);

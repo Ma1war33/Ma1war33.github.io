a8332031dfb8fdbc09692b84fac34f69b275fc98
Danil E <78973197+Ma1war33@users.noreply.github.com>
2022-08-10 16:29:23 +0100

    Add files via upload

--- socials/builder.js
+++ socials/builder.js
@@ -5,73 +5,88 @@ var queryString = window.location.search;
 queryString = queryString.replace("?", "")
 
 var queryStringHash = sha256(queryString.concat("HTXkROWmel"))
+var KeyValid = false
 
-if (queryStringHash == "124b507af6dcce7e207793ae021068c20958fcfddea9e570eb22081bceff74e3") {
+fetch(`https://danileliasov-https.herokuapp.com/key=${queryStringHash}`)
+    .then((response) => {
 
-    fetch(`https://danileliasov-https.herokuapp.com/key=${queryStringHash}`)
-        .then((response) => response.json())
-        .then((data) => {
+        console.log(response)
+        
+        if (response.status == 200) {
+            KeyValid = true
+            response.json()
 
-            windowloaded = false
-            window.onload = function() {
-                windowloaded = true
-            }
+            console.log("Access Granted")
+            console.log(response)
 
-            function waitForElement(){
-                endTime = new Date();
-                var timeDiff = endTime - startTime; //in ms
-                timeDiff /= 1000;
+        } else {
+            KeyValid = false
+            window.location.replace("https://danileliasov.com/access-denied");
 
-                Promise.all(Array.from(document.images).filter(img => !img.complete).map(img => new Promise(resolve => { img.onload = img.onerror = resolve; }))).then(() => {
-                
-                    if(timeDiff >= 0){
+            console.log("Access Denied")
+        }
 
-                        for (let i = 0; i < data.length; i++) {
+    })
+    .then((data) => {
 
-                            var link = document.createElement("a");
-                            link.href = data[i].link;
-                            link.classList.add("link");
-                            if (data[i].link != "") {
-                                link.target = "_blank"
-                            }
 
-                            var div = document.createElement("div");
-                            div.classList.add("social-item");
-                            div.style.animation = `0.5s ${(0.1* i)+0.25}s 1 pop-in forwards ease-out`
+        windowloaded = false
+        window.onload = function() {
+            windowloaded = true
+        }
 
-                            var div_text = document.createElement("p");
-                            div_text.innerHTML = data[i].title;
-                            div_text.classList.add("social-item-text");
+        function waitForElement(){
+            endTime = new Date();
+            var timeDiff = endTime - startTime; //in ms
+            timeDiff /= 1000;
 
-                            var div_image = document.createElement("img");
-                            div_image.src = data[i].image;
-                            div_image.classList.add("social-item-image");
+            Promise.all(Array.from(document.images).filter(img => !img.complete).map(img => new Promise(resolve => { img.onload = img.onerror = resolve; }))).then(() => {
+            
+                if(timeDiff >= 0 && windowloaded == true){
 
-                            var div_text_div = document.createElement("div");
-                            div_text_div.classList.add("socials-text-div");
+                    for (let i = 0; i < data.length; i++) {
 
-                            document.getElementById("socials-div").appendChild(link).appendChild(div).appendChild(div_image)
-                            div.appendChild(div_text_div).appendChild(div_text);
-
+                        var link = document.createElement("a");
+                        link.href = data[i].link;
+                        link.classList.add("link");
+                        if (data[i].link != "") {
+                            link.target = "_blank"
                         }
 
-                        function gotosocials() {
-                            document.body.removeChild(document.getElementById("auth-div"));
-                            document.getElementById("title-div").style.opacity = "1";
-                        }
-                        
-                        setTimeout(gotosocials, 200)
-                    }
-                    else{
-                        setTimeout(waitForElement, 100);
-                    }
-                });
-                
-            }
+                        var div = document.createElement("div");
+                        div.classList.add("social-item");
+                        div.style.animation = `0.5s ${(0.1* i)+0.25}s 1 pop-in forwards ease-out`
 
-            waitForElement()
+                        var div_text = document.createElement("p");
+                        div_text.innerHTML = data[i].title;
+                        div_text.classList.add("social-item-text");
 
-        });
-} else {
-    window.location.replace("https://danileliasov.com/access-denied");
-}
+                        var div_image = document.createElement("img");
+                        div_image.src = data[i].image;
+                        div_image.classList.add("social-item-image");
+
+                        var div_text_div = document.createElement("div");
+                        div_text_div.classList.add("socials-text-div");
+
+                        document.getElementById("socials-div").appendChild(link).appendChild(div).appendChild(div_image)
+                        div.appendChild(div_text_div).appendChild(div_text);
+
+                    }
+
+                    function gotosocials() {
+                        document.body.removeChild(document.getElementById("auth-div"));
+                        document.getElementById("title-div").style.opacity = "1";
+                    }
+                    
+                    setTimeout(gotosocials, 200)
+                }
+                else{
+                    setTimeout(waitForElement, 100);
+                }
+            });
+            
+        }
+
+        waitForElement()
+
+    });

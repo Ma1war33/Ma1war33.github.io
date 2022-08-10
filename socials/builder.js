8db5c6db5ddcb75917f6e12d08552e7d2c06489a
Danil E <78973197+Ma1war33@users.noreply.github.com>
2022-08-10 17:47:22 +0100

    Add files via upload

--- socials/builder.js
+++ socials/builder.js
@@ -100,16 +100,84 @@
         return result;
     };
 
+    function buildPage(data) {
+
+        console.log(data)
+
+        function waitForElement(){
+            endTime = new Date();
+            var timeDiff = endTime - startTime; //in ms
+            timeDiff /= 1000;
+
+            
+            if(timeDiff >= 0) {
+
+                for (let i = 0; i < data.length; i++) {
+
+                    var link = document.createElement("a");
+                    link.href = data[i].link;
+                    link.classList.add("link");
+                    if (data[i].link != "") {
+                        link.target = "_blank"
+                    }
+
+                    var div = document.createElement("div");
+                    div.classList.add("social-item");
+                    div.style.animation = `0.5s ${(0.1* i)+0.25}s 1 pop-in forwards ease-out`
+
+                    var div_text = document.createElement("p");
+                    div_text.innerHTML = data[i].title;
+                    div_text.classList.add("social-item-text");
+
+                    var div_image = document.createElement("img");
+                    div_image.src = data[i].image;
+                    div_image.classList.add("social-item-image");
+
+                    var div_text_div = document.createElement("div");
+                    div_text_div.classList.add("socials-text-div");
+
+                    document.getElementById("socials-div").appendChild(link).appendChild(div).appendChild(div_image)
+                    div.appendChild(div_text_div).appendChild(div_text);
+
+                }
+
+                function gotosocials() {
+                    document.body.removeChild(document.getElementById("auth-div"));
+                    document.getElementById("title-div").style.opacity = "1";
+                }
+                
+                setTimeout(gotosocials, 200)
+            }
+            else{
+                setTimeout(waitForElement, 100);
+            }
+            
+            
+        }
+
+        waitForElement()
+
+    }
+
+
     var queryString = window.location.search;
     queryString = queryString.replace("?", "")
 
-var queryStringHash = sha256(queryString.concat("HTXkROWmel"))
+    var queryStringHash = sha256(queryString.concat("HTXkROWmel"))
 
-    const rawResponse = await fetch(`https://danileliasov-https.herokuapp.com/key=${queryStringHash}`);
-    const content = await rawResponse.json();
+    try {
+        const rawResponse = await fetch(`https://danileliasov-https.herokuapp.com/key=${queryStringHash}`);
+        const content = await rawResponse.json();
+        console.log(content);
+
+        if (content.status == 200) {
+            buildPage(content)
+        }
+    } catch {
+        console.log(rawResponse.text())
+    }
     
-    console.log(content[0].title);
-    console.log(content[0].link);
+    
 })();
 
 function temp () {

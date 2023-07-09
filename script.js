9440234fa5b40060173be4828b34532673ec0896
Danil E <78973197+Ma1war33@users.noreply.github.com>
2023-07-09 23:45:27 +0100

    Add files via upload
    Portfolio 7

--- script.js
+++ script.js
@@ -1,243 +1,113 @@
-var gallery_div_f = document.getElementsByClassName('splash-background')[0]
-var gallery_div_b = document.getElementsByClassName('splash-background-two')[0]
 
-const bg_images = ['skyline.webp', 'flats.webp', 'skyline2.webp', 'maldives.webp', 'branch.webp']
-var current_bg_image = 0
-var alt = false
+loader = document.getElementsByClassName('loader')[0]
+nameWrapper = document.getElementsByClassName('name-wrapper')[0]
 
-sep1 = document.getElementsByClassName('sep1')[0]
-sep2 = document.getElementsByClassName('sep2')[0]
+document.onload = onload()
+function onload() {
+    setTimeout(() => { // decrease width after 1s
+        nameWrapper.style.width = '0px'
 
-setTimeout(() => {
-    gallery_div_f.children[0].addEventListener('load', () => {
-        sep1.style.animation = 'seperator_fade_1 2s 0.5s forwards'
-        sep2.style.animation = 'seperator_fade_2 2s 0.5s forwards'
-    })
-}, 250)
+        setTimeout(() => { // make translucent after 0.75s
+            loader.style.opacity = '0'
 
+            setTimeout(() => { // display none after 1s
+                loader.style.display = 'none'
+            }, 1000)
 
+        }, 750)
 
-
-fade_transition = 2 // sec
-transform_transition = 7 //sec
-
-gallery_div_b.style.transition = `opacity ${fade_transition}s, transform ${transform_transition}s linear`
-gallery_div_f.style.transition = `opacity ${fade_transition}s, transform ${transform_transition}s linear`
-
-gallery_div_f.style.transform = 'scale(1.2) translate(50px, 0px)'
-gallery_div_f.style.backgroundImage = 'url("Images/' + bg_images[0] + '")'
-
-setInterval(() => {
-    current_bg_image = (current_bg_image + 1) % bg_images.length
-
-    if (alt == false) {
-        alt = true
-
-        //gallery_div_b.style.backgroundImage = 'url("Images/' + bg_images[current_bg_image] + '")'
-
-        gallery_div_b.children[0].src = 'Images/' + bg_images[current_bg_image]
-
-        gallery_div_b.style.opacity = 1
-        gallery_div_b.style.transition = `opacity ${fade_transition}s, transform ${transform_transition}s linear`
-
-        gallery_div_b.style.transform = 'scale(1.2) translate(50px, 0px)'
-
-        gallery_div_f.style.opacity = 0
-
-        setTimeout(() => {
-            gallery_div_b.style.zIndex = 10
-            gallery_div_f.style.zIndex = 1
-            gallery_div_f.style.transition = 'none'
-            gallery_div_f.style.transform = 'scale(1.2) translate(-50px, 0px)'
-        }, fade_transition * 1000)
-
-    } else {
-        alt = false
-
-        //gallery_div_f.style.backgroundImage = 'url("Images/' + bg_images[current_bg_image] + '")'
-
-        gallery_div_f.children[0].src = 'Images/' + bg_images[current_bg_image]
-
-        gallery_div_f.style.opacity = 1
-        gallery_div_f.style.transition = `opacity ${fade_transition}s, transform ${transform_transition}s linear`
-
-        gallery_div_f.style.transform = 'scale(1.2) translate(50px, 0px)'
-
-        gallery_div_b.style.opacity = 0
-
-        setTimeout(() => {
-            gallery_div_f.style.zIndex = 10
-            gallery_div_b.style.zIndex = 1
-            gallery_div_b.style.transition = 'none'
-            gallery_div_b.style.transform = 'scale(1.2) translate(-50px, 0px)'
-        }, fade_transition * 1000)
-
-
-    }
-}, (transform_transition - fade_transition) * 1000)
-
-menu_expand = document.getElementsByClassName('menu-container')[0]
-menu_modal = document.getElementsByClassName('menu-modal')[0]
-
-var menu_open = false
-menu_expand.addEventListener('click', () => {
-    menu_open = menu_open ? false : true
-    if (menu_open == true) {
-        menu_modal.style.opacity = '0'
-        menu_modal.style.display = 'block'
-        setTimeout(() => {
-            menu_modal.style.animation = 'menu-open 1s forwards'
-        }, 10)
-    }
-    else {
-        menu_modal.style.animation = 'menu-close 1s forwards'
-        setTimeout(() => {
-            if (menu_open == false) {
-                menu_modal.style.display = 'none'
-            }
-        }, 1000)
-    }
-})
-
-links = document.getElementsByTagName('a')
-for (let i = 0; i < links.length; i++) {
-    links[i].addEventListener('click', () => {
-        menu_open = false
-        menu_modal.style.animation = 'menu-close 1s forwards'
-        setTimeout(() => {
-            if (menu_open == false) {
-                menu_modal.style.display = 'none'
-            }
-        }, 1000)
-    })
+    }, 500)
 }
-
-
-fade_anims = [
-    {
-        'name': 'welcome',
-        'elem': document.getElementsByClassName('text-container')[0],
-        'fadein': 35,
-        'fadeout': 999999,
-        'animationdelay': 0,
-    },
-    {
-        'name': 'projects',
-        'elem': document.getElementsByClassName('project-div')[0],
-        'fadein': 120,
-        'fadeout': 999999,
-        'animationdelay': 0,
-    },
-    {
-        'name': 'projects-preview',
-        'elem': document.getElementsByClassName('right-col')[0],
-        'fadein': 110,
-        'fadeout': 999999,
-        'animationdelay': 0,
-    },
-
-]
-
-document.addEventListener('scroll', () => { scroll_handler() })
-
-function scroll_handler() {
-    vh_p = window.innerHeight / 100
-
-    for (let i = 0; i < fade_anims.length; i++) {
-        if (scrollY / vh_p >= fade_anims[i]['fadein'] && scrollY / vh_p <= fade_anims[i]['fadeout']) {
-            fade_anims[i]['elem'].style.animation = `fade-in-up 1s ${fade_anims[i]['animationdelay']}s forwards`
-        } else if (scrollY / vh_p >= fade_anims[i]['fadeout']) {
-            fade_anims[i]['elem'].style.animation = `fade-out-down 1s ${fade_anims[i]['animationdelay']}s forwards`
-        }
-    }
-}
-
-scroll_handler()
 
 projects = [
     {
-        'name': '',
-        'coverimage': 'Images/gdp.webp',
-        'description': 'A website that allows the user to select five countries, whose rate of growth can then be compared by calculating the correlation coefficient between their change in GDP and presenting this as a correlation matrix.<br><br>Data is fetched from an https server and correlation is calculated server-side.<br><br>Check it out <a href="https://danileliasov.com/gdp" target="_blank">here</a>.',
-        'link': ''
+        'h1': 'GDP Growth Correlation Comparison Website',
+        'p': 'A website allowing comparison of growth between countries over a period of time by calculating a correlation between their change in GDP. The data is displayed in a correlation matrix, illustrating the relationships between countries.',
+        'href': 'https://danileliasov.com/gdp'
     },
     {
-        'name': '',
-        'coverimage': 'Images/tictactoe.webp',
-        'description': 'An online multiplayer tictactoe game.<br><br>Play <a href="https://danileliasov.com/tictactoe" target="_blank">here</a>.',
-        'link': ''
+        'h1': 'Maze Solver',
+        'p': 'A python program to solve mazes using either BFS (Breadth First Search) or DFS (Depth First Search). Mazes are inputted by holding them up to the camera, then selecting a start and end. Worked on in collaberation with <a href="https://zacnolan.com/" target="_blank">Zac Nolan.</a>',
+        'href': 'https://github.com/Lem0naise/maze-solver'
     },
     {
-        'name': '',
-        'coverimage': 'Images/wordle.webp',
-        'description': 'A python based version of wordle, played in the terminal.<br><br>Check it out <a href="https://github.com/Ma1war33/Wordle" target="_blank">here</a>.',
-        'link': ''
+        'h1': 'Noughts and Crosses',
+        'p': 'An online multiplayer noughts and crosses / tic-tac-toe game.',
+        'href': 'https://danileliasov.com/tictactoe'
     },
     {
-        'name': '',
-        'coverimage': 'Images/homepage.webp',
-        'description': 'This website was coded with HTML, CSS and Javascript without the use of any frameworks.',
-        'link': ''
+        'h1': 'Wordle',
+        'p': 'Wordle, implemented in python, played in the terminal.',
+        'href': 'https://github.com/Ma1war33/Wordle'
     },
-    {
-        'name': '',
-        'coverimage': 'Images/pi.webp',
-        'description': 'A few simple Python programs to approximate PI using different infinite series. The different speeds of convergence of different series can be measured by timing the execution of the programs.',
-        'link': ''
-    },
-    {
-        'name': '',
-        'coverimage': 'Images/morse.webp',
-        'description': 'A simple Python program to translate text to morsecode and back. The program automatically detects the direction of translation and outputs the result.<br><br>Check it out <a href="https://github.com/Ma1war33/MorseEncoder" target="_blank">here</a>.',
-        'link': ''
-    }
-
-
 ]
 
-project_cont = document.getElementsByClassName("project-div")[0]
-columns = [document.getElementsByClassName("col-1")[0], document.getElementsByClassName("col-2")[0], document.getElementsByClassName("col-3")[0]]
+projectsDiv = document.getElementsByClassName('projects')[0]
 
 for (let i = 0; i < projects.length; i++) {
-    project_div = document.createElement("div")
-    //project_img = document.createElement("img")
-    //project_img.classList.add('project-img')
-    //project_img.src = projects[i]['coverimage']
+    projectDiv = document.createElement('div')
+    projectDiv.classList.add('project')
 
-    //project_div.appendChild(project_img)
-    project_div.style.backgroundImage = 'url("' + projects[i]['coverimage'] + '")'
-    project_div.classList.add('project')
+    projectDiv.innerHTML = `<h1>${projects[i]['h1']}</h1><p>${projects[i]['p']}</p>`
+    projectDiv.onclick = () => { window.open(projects[i]['href']) }
 
-    console.log(((i + 1) % 3) - 1)
-    columns[i % 3].appendChild(project_div)
+    projectDiv.style.opacity = '0'
+    projectDiv.style.animation = `pop-in 0.8s ${i * 0.2 + 1.25}s forwards`
 
-    fade_anims.push(
-        {
-            'name': '',
-            'elem': project_div,
-            'fadein': 110,
-            'fadeout': 999999,
-            'animationdelay': Math.random() * 0.3,
-        }
-    )
-
-    project_div.addEventListener('mouseover', () => { project_handle_mouseover(i) })
+    projectsDiv.appendChild(projectDiv)
 }
 
-project_preview = document.getElementsByClassName('project-preview')[0]
-project_desc = document.getElementsByClassName('project-description')[0]
-function project_handle_mouseover(index) {
-    project_preview.style.backgroundImage = 'url("' + projects[index]['coverimage'] + '")'
-    project_desc.innerHTML = projects[index]['description']
+function mobileCheck() {
+    let check = false;
+    (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
+    return check;
+};
+
+const urlParams = new URLSearchParams(window.location.search);
+const loadMobile = urlParams.get('loadmobile') || true
+
+text = [document.getElementsByClassName('name')[0], document.getElementsByClassName('about-div')[0], document.getElementsByClassName('name')[0], document.getElementsByClassName('projects')[0]]
+if (mobileCheck() == true && loadMobile == true) {
+    console.log('LOADING WEBSITE FOR MOBILE')
+
+    document.getElementsByClassName('name-wrapper')[0].style.transform = 'scale(2)'
+
+    for (let i = 0; i < text.length; i++) {
+        text[i].style.fontSize = `${window.getComputedStyle(text[i], null).getPropertyValue('font-size').slice(0, -2) * 2}px`
+    }
+
+    headers = document.getElementsByTagName('h1')
+    for (let i = 0; i < headers.length; i++) {
+        headers[i].style.fontSize = '60px'
+    }
+
+    p = document.getElementsByTagName('p')
+    for (let i = 0; i < p.length; i++) {
+        p[i].style.fontSize = '40px'
+    }
+
+    a = document.getElementsByTagName('a')
+    for (let i = 0; i < a.length; i++) {
+        a[i].style.fontSize = '40px'
+    }
+
+    document.getElementsByClassName('navbar')[0].style.height = '200px'
+    document.getElementsByClassName('seperator-title')[0].style.display = 'none'
+
+    pop_up = document.createElement('div')
+    pop_up.classList.add('mobile-load-pop-up')
+
+    pop_up_inner = document.createElement('div')
+    pop_up_inner.innerHTML = '<h2>Website was loaded<br>for mobile.</h2><button class="reload" onclick=handleReloadButton()>Reload</button><button class="ok" onclick=handleOkButton()>Ok</button>'
+    pop_up.appendChild(pop_up_inner)
+
+    document.body.appendChild(pop_up)
 }
 
-// Smooth scrolling when clicking anchor link
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+function handleOkButton() {
+    document.getElementsByClassName('mobile-load-pop-up')[0].style.animation = 'notif-pop-down 0.25s 0s forwards'
+}
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+function handleReloadButton() {
+    window.location.replace('https://danileliasov.com?loadmobile=false')
+}
